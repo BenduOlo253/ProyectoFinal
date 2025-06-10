d827a6982a018f7cd80d0f3f2f7aeb0edbc5eca9
BenduOlo253 <jmanuelrb1@outlook.com>
2025-06-10 11:34:17 -0600

    Termino de la creacion de los DAOs

--- Modelo/datos/pacienteDAO.py
+++ Modelo/datos/pacienteDAO.py
@@ -1,32 +1,66 @@
 import sqlite3
-
-# Conexión a una base de datos (se crea si no existe)
-conexion = sqlite3.connect('/DataBases/Hospital.db')
-
-# Crear un cursor para ejecutar sentencias SQL
-cursor = conexion.cursor()
-
-# Crear una tabla (si no existe)
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS "Pacientes" ( 
-               "id" TEXT NOT NULL UNIQUE, 
-               "nombre" TEXT NOT NULL, 
-               "edad" INTEGER NOT NULL, 
-               "genero" TEXT NOT NULL, 
-               "motivo" TEXT NOT NULL, 
-               "gravedad" INTEGER NOT NULL, 
-               "fechadeingreso" TEXT NOT NULL, 
-               "atendido" INTEGER NOT NULL, PRIMARY KEY("id") 
-    );''')
+import os
 
 class PacienteDAO:
 
-    #Insertar datos del paciente a la base de datos.
+    @staticmethod
+    def __obtenerConexion():
+        ruta_db = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'Data', 'Hospital.db'))
+        return sqlite3.connect(ruta_db)
+
+    @staticmethod
+    def __crearTabla(cursor):
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Pacientes (
+                id TEXT NOT NULL UNIQUE,
+                nombre TEXT NOT NULL,
+                edad INTEGER NOT NULL,
+                genero TEXT NOT NULL,
+                motivo TEXT NOT NULL,
+                gravedad INTEGER NOT NULL,
+                fechadeingreso TEXT NOT NULL,
+                atendido INTEGER NOT NULL,
+                PRIMARY KEY(id)
+            );
+        ''')
+
+    @staticmethod
+    def  obtenerPacientes():
+        conn = PacienteDAO.__obtenerConexion()
+        cursor = conn.cursor()
+        PacienteDAO.__crearTabla(cursor)
+        cursor.execute('SELECT * FROM Pacientes')
+        filas = cursor.fetchall()
+        conn.close()
+        return filas
+    
     @staticmethod
     def insertarPaciente(idPaciente, nombre, edad, genero, motivo, gravedad, fechadeingreso, atendido):
+        conn = PacienteDAO.__obtenerConexion()
+        cursor = conn.cursor()
+        PacienteDAO.__crearTabla(cursor)
         cursor.execute('''
             INSERT INTO Pacientes (id, nombre, edad, genero, motivo, gravedad, fechadeingreso, atendido)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         ''', (idPaciente, nombre, edad, genero, motivo, gravedad, fechadeingreso, atendido))
-        conexion.commit()
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def actualizarPaciente(nombre, edad, genero, motivo, gravedad, fechadeingreso, atendido, idPaciente):
+        conn = PacienteDAO.__obtenerConexion()
+        cursor = conn.cursor()
+        cursor.execute('''
+            UPDATE Pacientes
+            SET nombre = ?, edad = ?, genero = ?, motivo = ?, gravedad = ?, fechadeingreso = ?, atendido = ?
+            WHERE id = ?''', (nombre, edad, genero, motivo, gravedad, fechadeingreso, atendido, idPaciente))
+        conn.commit()
+        conn.close()
     
+    @staticmethod
+    def eliminarPaciente(idPaciente):
+        conn = PacienteDAO.__obtenerConexion()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM Pacientes WHERE id = ?', (idPaciente))
+        conn.commit()
+        conn.close()
