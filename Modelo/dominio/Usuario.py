9ba57634c5ff95299e8d8d5d6d3375edf2e9009d
BenduOlo253 <jmanuelrb1@outlook.com>
2025-06-10 00:16:50 -0600

    Modificacion de las entidades paciente y usuario

--- Modelo/dominio/Usuario.py
+++ Modelo/dominio/Usuario.py
@@ -1,8 +1,36 @@
 # Clase que representa a un paciente con atributos básicos
 class Usuario:
-    def __init__(self, id, nombre, contraseña, rol):
-        self.id = id                # Identificador único del paciente
-        self.nombre = nombre        # Nombre del paciente
-        self.contraseña = contraseña
-        self.rol = rol
-        
+    def __init__(self, idUsuario, nombre, contraseña, rol):
+        self.__idUsuario = idUsuario                 # Identificador único del usuario, puede ser su numero de trabajador.
+        self.__nombre = nombre         # Nombre del usuario.
+        self.__contraseña = contraseña # Contraseña del usuario.
+        self.__rol = rol              # Rol del usuario (por ejemplo, 'admin', 'doctor', 'enfermero', etc.).
+    
+    # Getters
+    def getId(self):
+        return self.__idUsuario
+    
+    def getNombre(self):
+        return self.__nombre
+    
+    def getContraseña(self):
+        return self.__contraseña
+    
+    def getRol(self):
+        return self.__rol
+
+
+    # Setters        
+    def setId(self, idUsuario):
+        self.__idUsuario = idUsuario
+    
+    def setNombre(self, nombre):
+        self.__nombre = nombre
+    def setContraseña(self, contraseña):
+        self.__contraseña = contraseña
+    def setRol(self, rol):
+        self.__rol = rol
+
+    # Método para representar el objeto como una cadena de texto.
+    def __str__(self):
+        return f"Usuario(idUsuario:{self.__idUsuario}, nombre:{self.__nombre}, rol={self.__rol})"
