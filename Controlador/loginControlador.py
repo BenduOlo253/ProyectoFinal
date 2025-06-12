f982bb67ddc1340f3052119b275e7ac07ca35c6c
BenduOlo253 <jmanuelrb1@outlook.com>
2025-06-11 19:57:00 -0600

    Creacion del controlador de la ventana login

---
+++ Controlador/loginControlador.py
@@ -0,0 +1,14 @@
+from Modelo.servicio.usuarioService import UsuarioService
+from tkinter import messagebox
+
+class LoginControlador:
+    ### Funciones de los botones
+    def iniciar_sesion(idUsuario, contraseña):
+        if UsuarioService.autenticarUsuario(idUsuario, contraseña):
+            nombreUsuario = (UsuarioService.buscarUsuario(idUsuario)[0])[1]
+            messagebox.showinfo("Inicio de sesion exitoso!", f"Bienvenido {nombreUsuario}!")
+        else:
+            messagebox.showerror("Error de inicio de sesión", "ID de usuario o contraseña incorrectos.")
+
+    def registrarse():
+        messagebox.showinfo("Registro", "Función de registro aún no implementada.")
