8808f9f13cf6207a02b2a340d6593e38f854807a
BenduOlo253 <jmanuelrb1@outlook.com>
2025-06-14 15:33:14 -0600

    xd

--- structures/tablaHash.py
+++ structures/tablaHash.py
@@ -1,22 +1,53 @@
-from structures.listaEnlazada import ListaEnlazada
-
-class TablaHash:
+class TablaHashDoble:
     def __init__(self, tamaño=10):
         self.tamaño = tamaño
-        self.tabla = [ListaEnlazada() for _ in range(tamaño)]
+        self.tabla = [None] * tamaño
+        self.valores = [None] * tamaño  # para claves
 
-    def _hash(self, clave):
+    def _hash1(self, clave):
         return sum(ord(c) for c in clave) % self.tamaño
 
+    def _hash2(self, clave):
+        # Segunda función hash, evita múltiplos de tamaño
+        return 1 + (sum(ord(c) for c in clave) % (self.tamaño - 1))
+
+    def _sonIguales(self, a, b):
+        return a == b
+
     def insertarValor(self, clave, valor):
-        indice = self._hash(clave)
-        self.tabla[indice].insertar(clave, valor)
+        idx = self._hash1(clave)
+        salto = self._hash2(clave)
+
+        for i in range(self.tamaño):
+            nuevo_idx = (idx + i * salto) % self.tamaño
+            if self.tabla[nuevo_idx] is None or self.valores[nuevo_idx] == clave:
+                self.tabla[nuevo_idx] = valor
+                self.valores[nuevo_idx] = clave
+                return
+        raise Exception("Tabla Hash llena")
 
     def obtenerValor(self, clave):
-        indice = self._hash(clave)
-        return self.tabla[indice].buscar(clave)
-    
+        idx = self._hash1(clave)
+        salto = self._hash2(clave)
+
+        for i in range(self.tamaño):
+            nuevo_idx = (idx + i * salto) % self.tamaño
+            if self.valores[nuevo_idx] == clave:
+                return self.tabla[nuevo_idx]
+            if self.tabla[nuevo_idx] is None:
+                break
+        return None
 
     def eliminarValor(self, clave):
-        indice = self._hash(clave)
-        return self.tabla[indice].eliminar(clave)
+        idx = self._hash1(clave)
+        salto = self._hash2(clave)
+
+        for i in range(self.tamaño):
+            nuevo_idx = (idx + i * salto) % self.tamaño
+            if self.valores[nuevo_idx] == clave:
+                self.tabla[nuevo_idx] = None
+                self.valores[nuevo_idx] = None
+                return True
+            if self.tabla[nuevo_idx] is None:
+                break
+        return False
